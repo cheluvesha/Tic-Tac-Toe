97bf2db98b76eb3caff2277764be6809dc1f77aa
cheluvesha <cheluvesh.b@gmail.com>
2020-09-13 21:26:43 +0530

    [CHELUVESHA]Added: Display function

--- TicTacToe.java
+++ TicTacToe.java
@@ -12,14 +12,21 @@ class TicTacToe {
 	//Method to initialise board
 	public static void initBoard() {
 		board = new ArrayList<ArrayList<String> >();
-		board.add(new ArrayList<String>(Arrays.asList("-","-","-")));
-		board.add(new ArrayList<String>(Arrays.asList("-","-","-")));
-		board.add(new ArrayList<String>(Arrays.asList("-","-","-")));
+		board.add(new ArrayList<String>(Arrays.asList("1","2","3")));
+		board.add(new ArrayList<String>(Arrays.asList("4","5","6")));
+		board.add(new ArrayList<String>(Arrays.asList("7","8","9")));
+	}
+
+	public static void displayBoard() {
+		System.out.println(board.get(0).get(0)+ "|" +board.get(0).get(1)+ "|" +board.get(0).get(2));
+		System.out.println(board.get(1).get(0)+ "|" +board.get(1).get(1)+ "|" +board.get(1).get(2));
+		System.out.println(board.get(2).get(0)+ "|" +board.get(2).get(1)+ "|" +board.get(2).get(2));
 	}
 
 	//Method to choose who goes first
 	public static void chooseFirst(int yourChoice) {
-		int randomChoice = (int) Math.floor(Math.random()*10)%2+1;
+		Random random =new Random();
+		int randomChoice = random.nextInt(2);
 		if (randomChoice == yourChoice) {
 			whoseTurn = 1;
 			System.out.println("You won the toss.");
@@ -48,7 +55,7 @@ class TicTacToe {
 		int userSymbolChoice;
 		if (playerFlag == flag) {
 			Scanner input = new Scanner(System.in);
-			System.out.print("Select your choice (1: X, 2: O): ");
+			System.out.print("Select your symbol choice (1: X, 2: O): ");
 			userSymbolChoice = input.nextInt();
 			secChooseSymbol(userSymbolChoice);
 		}
@@ -58,16 +65,23 @@ class TicTacToe {
 		}
 	}
 
-	//Main
-	public static void main(String[] args) {
+	//Method perform first run of game
+	public static void firstRun() {
 		//take user input
 		Scanner input = new Scanner(System.in);
-		System.out.print("Select your choice (1:Head, 2:Tail): ");
+		System.out.print("Select your toss choice (1:Head, 2:Tail): ");
 		int userChoice = input.nextInt();
 		chooseFirst(userChoice);
+		System.out.println("Player symbol: "+playerSymbol+ "\nCPU Symbol : "+cpuSymbol+"\n");
 		initBoard();
-		System.out.println(board);
+		System.out.println("Fresh Board:");
+		displayBoard();
+		System.out.println("----------------------------------------");
+	}
 
-		System.out.println("Player symbol: "+playerSymbol+ " CPU Symbol : "+cpuSymbol);
+	//Main
+	public static void main(String[] args) {
+		//initial run to set all variables
+		firstRun();
 	}
 }
