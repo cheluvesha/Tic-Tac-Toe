9b8dec2922daed81942fcd4a3fbb06226ae84f92
cheluvesha <cheluvesh.b@gmail.com>
2020-09-13 20:59:37 +0530

    [CHELUVESHA]Added: Resetting a board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,2 +1,24 @@
 // Welcome to TicTacToe 
 
+import java.util.*;
+
+class TicTacToe {
+
+	//declaring a gobal tic tac toe board
+	private static ArrayList<ArrayList<String> > board;
+
+	//Method to initialise board
+	public static void initBoard() {
+		board = new ArrayList<ArrayList<String> >();
+		board.add(new ArrayList<String>(Arrays.asList("-","-","-")));
+		board.add(new ArrayList<String>(Arrays.asList("-","-","-")));
+		board.add(new ArrayList<String>(Arrays.asList("-","-","-")));
+	}
+
+	public static void main(String[] args) {
+
+		initBoard();
+		System.out.println(board);
+
+	}
+}
