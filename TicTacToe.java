5db5b811b34dc8502792eadba69d884657c229ff
cheluvesha <cheluvesh.b@gmail.com>
2020-09-13 21:54:43 +0530

    [CHELUVESHA]Added: Determining the game progress

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 class TicTacToe {
 
-	//declaring a gobal tic tac toe board
+	//declaring a gobal tic tac toe board and variables
 	private static ArrayList<ArrayList<String> > board;
 	private static int whoseTurn = 0; // to monitor turns 1 --> player and 0 --> cpu
 	private static int playerFlag = 1; // to check if player won(1) toss or cpu(0)
 	private static String playerSymbol = "";
 	private static String cpuSymbol = "";
+	private static int isGameIsGoing = 1; // if 1 --> game is going and  0 --> end game
 
 	//Method to initialise board
 	public static void initBoard() {
@@ -78,9 +79,82 @@ class TicTacToe {
 		System.out.println("----------------------------------------");
 	}
 
-	//Main
+	//Method to check row for win
+	public static int checkRow(){
+		int rowFlag = 0;
+		if (board.get(0).get(0) == board.get(0).get(1) && board.get(0).get(1) == board.get(0).get(2))
+			rowFlag = 1;
+		else if (board.get(1).get(0) == board.get(1).get(1) && board.get(1).get(1) == board.get(1).get(2))
+			rowFlag = 1;
+		else if (board.get(2).get(0) == board.get(2).get(1) && board.get(2).get(1) == board.get(2).get(2))
+			rowFlag = 1;
+		return rowFlag;
+	}
+
+	//Method to check column for win
+	public static int checkCol(){
+		int colFlag = 0;
+		if (board.get(0).get(0) == board.get(1).get(0) && board.get(1).get(0) == board.get(2).get(0))
+			colFlag = 1;
+		else if (board.get(0).get(1) == board.get(1).get(1) && board.get(1).get(1) == board.get(2).get(1))
+			colFlag = 1;
+		else if (board.get(0).get(2) == board.get(1).get(2) && board.get(1).get(2) == board.get(2).get(2))
+			colFlag = 1;
+		return colFlag;
+	}
+
+	//Method to check diagonal for win
+	public static int checkDia(){
+		int diaFlag = 0;
+		if (board.get(0).get(0) == board.get(1).get(1) && board.get(1).get(1) == board.get(2).get(2))
+			diaFlag = 1;
+		else if (board.get(0).get(2) == board.get(1).get(1) && board.get(1).get(1) == board.get(2).get(0))
+			diaFlag = 1;
+		return diaFlag;
+	}
+
+	//Method to check win or not
+	public static int checkWin(){
+		if (checkRow() == 1 || checkCol() == 1 || checkDia() == 1)
+			return 1;
+		else
+			return 0;
+	}
+
+	//Method to check tie or not
+	public static int checkTie(){
+		int tieFlag = 0;
+		if (board.get(0).get(0) != "1" && board.get(0).get(1) != "2" && board.get(0).get(2) != "3") {
+			if (board.get(1).get(0) != "4" && board.get(1).get(1) != "5" && board.get(1).get(2) != "6") {
+				if (board.get(2).get(0) != "7" && board.get(2).get(1) != "8" && board.get(2).get(2) != "9")
+					tieFlag = 1;
+			}
+		}
+		return tieFlag;
+	}
+
+	//Method to check game is going or not
+	public static void checkGameIsGoing() {
+		int winFlag = checkWin();
+		int tieFlag = checkTie();
+		if (winFlag == 1 || tieFlag == 1)
+			isGameIsGoing = 0;
+		System.out.println( winFlag +", "+ tieFlag);
+	}
+
+	//Method to change player
+	public static void changePlayer() {
+		if (whoseTurn == 0)
+			whoseTurn = 1;
+		else
+			whoseTurn = 0;
+	}
+
+	//Main function
 	public static void main(String[] args) {
 		//initial run to set all variables
 		firstRun();
+		checkGameIsGoing();
+		changePlayer();
 	}
 }
