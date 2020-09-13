70bb7139f0f7f5335fce82a7cfe7d1db0460366c
cheluvesha <cheluvesh.b@gmail.com>
2020-09-13 21:14:07 +0530

    [CHELUVESHA]Added: Toss function

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,10 @@
-// Welcome to TicTacToe 
-
 import java.util.*;
 
 class TicTacToe {
 
 	//declaring a gobal tic tac toe board
 	private static ArrayList<ArrayList<String> > board;
+	private static int whoseTurn = 0;
 
 	//Method to initialise board
 	public static void initBoard() {
@@ -15,13 +14,28 @@ class TicTacToe {
 		board.add(new ArrayList<String>(Arrays.asList("-","-","-")));
 	}
 
-	public static void main(String[] args) {
-
-		initBoard();
-		Iterator iter = board.iterator();
-		while(iter.hasNext()){
-			System.out.println(iter.next());
+	//Method to choose who goes first
+	public static void chooseFirst(int yourChoice) {
+		Random random = new Random();
+		int randomChoice = random.nextInt(2);
+		if (randomChoice == yourChoice) {
+			whoseTurn =  randomChoice;
+			System.out.println("You won the toss.");
 		}
+		else {
+			whoseTurn =  randomChoice;
+			System.out.println("CPU won the toss.");
+		}
+	}
 
+	//Main
+	public static void main(String[] args) {
+		//take user input
+		Scanner input = new Scanner(System.in);
+		System.out.print("Select your choice (1:Head, 2:Tail): ");
+		int userChoice = input.nextInt();
+		chooseFirst(userChoice);
+		initBoard();
+		System.out.println(board);
 	}
 }
